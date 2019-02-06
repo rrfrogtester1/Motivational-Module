0e7570a4fed8b09956b97c4afb0510d1216cc048
rrfrogtester1 <47203037+rrfrogtester1@users.noreply.github.com>
2019-02-05 20:20:03 -0800

    Added PyPI Distribution

--- my_project/setup.py
+++ my_project/setup.py
@@ -5,7 +5,14 @@ __version__ = "0.0.1"
 __description__ = "a Python module to motivate you"
 __packages__ = ["motivational"]
 __author__ = "Riley Ryan"
-__author_email__ = "I'm not telling you my email!"
+__author_email__ = "nezofuvo@maxmail.info"
+__classifiers__ = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Education",
+    "Programming Language :: Python :: 3",
+]
+__url__ = "https://github.com/rrfrogtester1/Motivational-Module/"
+__keywords__ = ["motivation", "keep going!", "you got it!"]
 
 setup(
     name = __project__,
@@ -13,5 +20,8 @@ setup(
     description = __description__,
     packages = __packages__,
     author = __author__,
-    email = __author_email__
+    author_email = __author_email__,
+    classifiers = __classifiers__,
+    url = __url__,
+    keywords = __keywords__,
 )
